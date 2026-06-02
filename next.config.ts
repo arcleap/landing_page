676a6edee043e1c0a6545b16e6981c652662e136
Jin Miao <279829493+ArcLeapJin@users.noreply.github.com>
2026-06-01 20:06:20 -0700

    Configure Next.js rewrite for /signals to map to static index.html

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/signals",
+        destination: "/signals/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
